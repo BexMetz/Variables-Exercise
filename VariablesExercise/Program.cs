cdd673a3fcc4cc432e89a1ceccdab5c5b2d6f6a8
Bex <bexmetzger@gmail.com>
2023-04-09 10:09:21 -0700

    dog variable declaration/interpolation

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string dogName; //Declaration syntax
+
+            dogName = "Doc"; //Initialized -- assigned a value
+
+            int dogAge = 3; //Declaring and Initializing
+
+            char firstInitial = 'D';
+
+            bool isHungry = true;
+
+            double dogWeight = 82.3;
+
+            decimal dogHeight = 16.7m;
+
+            Console.WriteLine($"Hi! My Dogs name is {dogName} so his first initial is {firstInitial} and he is about to turn {dogAge} years old.");
+            Console.WriteLine($"He weighs {dogWeight} pounds and he is {dogHeight} inches tall.");
+            Console.WriteLine($"It is {isHungry.ToString().ToLower()} he is always hungry!");
         }
     }
 }
